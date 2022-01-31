90cbf8b46af764162025a94e15b3762fc6628ff8
Vlad Zusko <vladzvu11@gmail.com>
2022-02-01 00:18:23 +0200

    fix: fixed button component

--- src/components/basic/button/index.jsx
+++ src/components/basic/button/index.jsx
@@ -3,19 +3,43 @@ import styled from 'styled-components';
 import theme from 'styled-theming';
 import { basicStyles, lightTheme, darkTheme } from 'styles';
 
-const backgroundColor = theme('mode', {
+const color = theme('mode', {
   dark: darkTheme.colors.basic,
+  light: lightTheme.colors.secondary,
+});
+
+const contrastColor = theme('mode', {
+  dark: darkTheme.colors.secondary,
   light: lightTheme.colors.basic,
 });
 
 const StyledButton = styled.button`
+  cursor: pointer;
   padding: ${basicStyles.gutter}px;
-  background-color: ${backgroundColor};
+  background: none;
+  border: 1px solid ${color};
+  color: ${color};
   border-radius: ${basicStyles.radius}px;
+  transition: ${basicStyles.time}s;
+
+  &:hover {
+    background-color: ${color};
+    color: ${contrastColor};
+  }
 `;
 
-const Button = ({ text, className }) => {
-  return <StyledButton className={className}>{text}</StyledButton>;
+const Button = ({ text, className, onClick }) => {
+  const clickHandler = (event) => {
+    if (onClick instanceof Function) {
+      onClick(event);
+    }
+  };
+
+  return (
+    <StyledButton className={className} onClick={clickHandler}>
+      {text}
+    </StyledButton>
+  );
 };
 
 export default Button;
